f6465460592beb705b97ca06fde4f71ff91d1456
Azat Akmyradov <azat@akmyradov.me>
2025-06-18 14:44:23 -0400

    Shared chat collaboration

--- routes/channels.php
+++ routes/channels.php
@@ -5,5 +5,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('chat.{chat}', function (User $user, string $chatId) {
-    return $user->id === Chat::find($chatId)?->user_id;
+    $chat = Chat::find($chatId);
+
+    return $user->id === $chat?->user_id || $chat->is_public;
 });
