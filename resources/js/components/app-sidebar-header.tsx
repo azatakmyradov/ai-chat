d98feaffc7445b373322bc9f6e33408833e524b3
Azat Akmyradov <azakmyradov@rklesolutions.com>
2025-06-18 02:42:43 -0400

    Fixed header in mobile

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -1,5 +1,6 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import { SidebarTrigger, useSidebar } from '@/components/ui/sidebar';
+import { cn } from '@/lib/utils';
 import { type BreadcrumbItem as BreadcrumbItemType } from '@/types';
 
 export function AppSidebarHeader({ breadcrumbs = [] }: { breadcrumbs?: BreadcrumbItemType[] }) {
@@ -7,7 +8,12 @@ export function AppSidebarHeader({ breadcrumbs = [] }: { breadcrumbs?: Breadcrum
 
     return (
         <header className="flex h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/50 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
-            <div className="flex gap-2 items-center">
+            <div
+                className={cn(
+                    'flex items-center gap-2',
+                    isMobile && 'fixed top-0 right-0 left-0 z-50 bg-background bg-background/50 p-4 backdrop-blur-md',
+                )}
+            >
                 {isMobile && <SidebarTrigger className="-ml-1" />}
                 <Breadcrumbs breadcrumbs={breadcrumbs} />
             </div>
